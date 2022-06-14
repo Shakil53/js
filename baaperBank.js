6e488b61d4be490d369e5f97900157929bba0e16
Shakil53 <shakilahmed.dcc@gmail.com>
2022-06-14 19:37:34 +0600

    first commit

---
+++ baaperBank.js
@@ -0,0 +1,7 @@
+document.getElementById('login-submit').addEventListener('click', function ()
+{
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const userpassword = passwordField.value;
+})
